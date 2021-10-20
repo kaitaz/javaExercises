db970c73d817126c97a88e63c3b56d22cdfafd1f
Vio <Vio@Vio>
2021-10-20 20:36:55 +0300

    ch14

--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,5 @@ module JavaFX {
 	requires javafx.controls;
 	requires javafx.graphics;
 	
-	opens application to javafx.graphics, javafx.fxml;
 	opens ch_14_JavaFX_Basics to javafx.graphics, javafx.fxml;
 }
