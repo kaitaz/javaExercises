fc70bc19e477e2955942d5e76deaf39570952e34
Vio <Vio@192.168.138.1>
2022-06-02 09:19:16 +0300

    tw

--- src/module-info.java
+++ src/module-info.java
@@ -3,12 +3,25 @@ module JavaFX {
 	requires javafx.graphics;
 	requires javafx.base;
 	
-	opens ch_14_JavaFX_Basics to javafx.graphics, javafx.fxml;
+	/*opens ch_14_JavaFX_Basics to javafx.graphics, javafx.fxml;
 	opens ch_15_EventDrivenProgramming_and_Animation to javafx.graphics, javafx.fxml;
 	opens ch_16_JavaUI_Controls_and_Multimedia to javafx.graphics, javafx.fxml;
 	opens ch_17_Binary_IO to javafx.graphics, javafx.fxml;
 	opens ch_18_Recursion to javafx.graphics, javafx.fxml;
 	opens ch_19_Generics to javafx.graphics, javafx.fxml;
 	
-	opens ImportedClass to javafx.graphics, javafx.fxml;
+	opens ch_2_Elementary to javafx.graphics, javafx.fxml;
+	opens ch_3_Selection to javafx.graphics, javafx.fxml;
+	opens ch_4_Math_Char_String to javafx.graphics, javafx.fxml;
+	opens ch_5_Loops to javafx.graphics, javafx.fxml;
+	opens ch_6_Methods to javafx.graphics, javafx.fxml;
+	opens ch_7_Single_Dim_Arrays to javafx.graphics, javafx.fxml;
+	opens ch_8_Multidim_Array to javafx.graphics, javafx.fxml;
+	opens ch_9_Object_and_Classes to javafx.graphics, javafx.fxml;
+	opens ch_10_Object_Oriented_Thinking to javafx.graphics, javafx.fxml;
+	opens Ch_11_Inheritance_and_Polimorphism to javafx.graphics, javafx.fxml;
+	opens ch_12_ExceptionHandling_TextIO to javafx.graphics, javafx.fxml;
+	opens ch_13_AbstractClasses_and_Interfeces to javafx.graphics, javafx.fxml;
+	opens Imported_Class to javafx.graphics, javafx.fxml;*/
+	//opens  to javafx.graphics, javafx.fxml;
 }
